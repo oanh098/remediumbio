2f82c43487ada137d4a64bc5e81e2a1d833d722d
oanh098 <thuyoanh21790@gmail.com>
2019-10-14 18:55:15 +0700

    commit 20191014

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,13 @@
+<header id="ttr_header">
+    <div id="ttr_header_inner">
+        <div class="ttr_header_logo">
+        </div>
+        <div class="ttr_header_element_alignment container">
+        </div>
+        <div class="ttr_slogan_position">
+            <h1 class="ttr_slogan_style">
+                Call Us +1-587-487-572215874876186    |    info@remediumpharmacy.com
+            </h1>
+        </div>
+    </div>
+</header>
